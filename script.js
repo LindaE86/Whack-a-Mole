76494a24b151eb45558c1e9f9ddb9711ee850b68
LindaE86 <linda.malmborg@hotmail.se>
2024-09-09 14:32:36 +0200

    timer, click

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ document.addEventListener(
     "DOMContentLoaded", function () {
         const holes =
             document.querySelectorAll(".hole");
-<<<<<<< Updated upstream
-=======
         const scoreDisplay =
             document.getElementById("score");
         const timerDisplay =
             document.getElementById("timer");
 
->>>>>>> Stashed changes
         let bord;
         let startBtn; // används för reset och start
         let timer = 60;
