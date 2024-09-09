469e3438f49ac73df7b15903bf2e8d14f5a8237a
LindaE86 <linda.malmborg@hotmail.se>
2024-09-09 14:31:06 +0200

    timer, click

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ document.addEventListener(
     "DOMContentLoaded", function () {
         const holes =
             document.querySelectorAll(".hole");
+<<<<<<< Updated upstream
+=======
+        const scoreDisplay =
+            document.getElementById("score");
+        const timerDisplay =
+            document.getElementById("timer");
+
+>>>>>>> Stashed changes
         let bord;
         let startBtn; // används för reset och start
-        let timeLeft = 60;
+        let timer = 60;
         let score = 0;
         let reactionTime = [];
         let timeUp = false;
+
+        function handleMoleClick() {
+            if (!gameOver) {
+                score++;
+                scoreDisplay.textContent = `Score: ${score}`;
+            }
+            this.classList.remove('mole');
+        }
