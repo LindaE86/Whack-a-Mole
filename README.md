76dba807cab3d2e7590838ab0f97efab1da4f8e2
MalinStal <malinstal.94@gmail.com>
2024-09-09 10:11:21 +0200

    display for time and a timer function

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 Grundläggande kodhygien:
 
-Formatera koden med indrag så att den är lättläst.
+Formatera koden med indrag så att den är lättläst. indentering - 4 space
 Använd tydliga variabelnamn.
 Kommentera koden så att andra utvecklare snabbt kan förstå den.
 Hantera källkoden i git. Använd gärna flera grenar.
 Skriv commit-beskrivningar som tydligt förklarar vad som har ändrats (kort/några rader per
 commit).
 
-indentering - 4 space
--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 let bord; 
 let startBtn ; // används för reset och start
 let hole ;
+let displayHere = document.getElementById("timer");
 
 let timeLeft = 60;
 let score = 0;
 let reactionTime = [];
 let timeUp = false;
+
+// använts så här = CountDown(timeLeft, displayHere);
+function CountDown(time, display){  
+    
+    let timer = time, minutes, seconds;
+    timerInterval = setInterval(() =>{
+      
+
+        minutes = parseInt(timer / 60, 10); 
+        seconds =  parseInt(timer % 60, 10); 
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        console.log( minutes + ":" + seconds)
+        display.innerHTML = `${minutes}:${seconds}`; 
+
+        if(--timer < 0){
+            clearInterval(timerInterval);
+            timeUp = true;
+        }
+
+    }, 1000);
+}
