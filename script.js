37f809e67f01f8b93789809b6d0c1c3cff4f0e49
MalinStal <malinstal.94@gmail.com>
2024-09-10 09:43:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,6 @@ let score = 0;
 let reactionTime = [];
 let timeUp = false;
 
-document.addEventListener(
-    "DOMContentLoaded", function () {
-        const holes =
-            document.querySelectorAll(".hole");
-        let bord;
-        let startBtn; // används för reset och start
-        let timeLeft = 60;
-        let score = 0;
-        let reactionTime = [];
-        let timeUp = false;
-)}
-
 // använts så här = CountDown(timeLeft, displayHere);
 function CountDown(time, display){  
     
@@ -43,3 +31,4 @@ function CountDown(time, display){
 
     }, 1000);
 }
+
